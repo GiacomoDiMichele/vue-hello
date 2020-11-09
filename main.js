83f93c9b9694bf118fb44e49d0df1a55a0546246
Giacomo <giacomo.1936@gmail.com>
2020-11-09 10:07:18 +0100

    aggiunta img

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var titoloVue = new Vue (
     {
         el: '#root',
         data: {
-            titolo: 'Titolo Vue'        }
+            titolo: 'Titolo Vue',
+            immagine: 'https://cameranation.it/wp-content/uploads/2019/06/beach-1761410_1280-800x448.jpg'   
+           }
     }
 );
